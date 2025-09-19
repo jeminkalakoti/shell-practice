b8fc34e2e7db7b4c772bb0b69d58059e37146b81
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-19 01:58:44 -0500

    Greetings modified

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="Ande"
+PERSON2="Kalakoti"
+
+echo "$PERSON1:: Hello, how are you?"
+echo "$PERSON2:: I am good, thank you! How about you $PERSON1?"
+echo "$PERSON1:: I'm doing well, thanks for asking $PERSON2!"
+echo "$PERSON2:: That's great to hear!"
