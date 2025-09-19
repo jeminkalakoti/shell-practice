5217a20a09af3de397affb06664b8d1a93378898
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-19 11:56:33 -0500

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Please enter your PIN number"
-read PIN # Read user input and store it in the variable PIN
+read -s PIN # Read user input and store it in the variable PIN
 
 echo "You have entered PIN: $PIN"
