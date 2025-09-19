384f6701844d78f85212a0548b2aba73348a6570
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-19 11:19:33 -0500

    Welcome Message

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello, welcome to USA $PERSON2!"
+echo "$PERSON2:: Thank you so much $PERSON1, I am excited to be here!"
+echo "$PERSON1:: I hope you have a great experience here $PERSON2!"
+echo "$PERSON2:: I am sure I will, thanks again $PERSON1!"
