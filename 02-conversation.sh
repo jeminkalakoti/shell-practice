93710a86c9995e4917baa348dda23d16fb6084da
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-19 01:44:23 -0500

    Greetings

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Lakshmi:: Hello, how are you?"
+echo "Jemin:: I am good, thank you! How about you?"
+echo "Lakshmi:: I'm doing well, thanks for asking!"
+echo "Jemin:: That's great to hear!"
