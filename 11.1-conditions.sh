d5d83c7edac0fbfd56cfea5559b809657530a007
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-23 17:33:40 -0500

    about how to use conditions prime number

--- 11.1-conditions.sh
+++ 11.1-conditions.sh
@@ -5,6 +5,16 @@ echo "Enter a number:"
 read NUMBER
 
 if [ $NUMBER -le 1 ]; then
-  echo "The number $NUMBER is not prime"
+  echo "The number $NUMBER is not prime number"
   exit 0
 fi
+
+#To check if the number is prime
+for (( i=2; i<=NUMBER/2; i++ )); do
+  if [ $((NUMBER % i)) -eq 0 ]; then
+    echo "The number $NUMBER is not prime number"
+    exit 0
+  fi
+done    
+
+echo "The number $NUMBER is prime number"
