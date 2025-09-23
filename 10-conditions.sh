9e71ad54065aaa1a8a5b4dadbb829d1f6d9a7f54
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-23 15:16:58 -0500

    variables

---
+++ 10-conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#To check if a given number is less than 10
+
+NUMBER=$1
+if [ $NUMBER -lt 10 ]; then
+  echo "The number $NUMBER is less than 10"
+else
+  echo "The number $NUMBER is not less than 10"
+fi
+
+#-gt : greater than
+#-lt : less than
+#-eq : equal to
+#-ne : not equal to
+#-ge : greater than or equal to
+#-le : less than or equal to
