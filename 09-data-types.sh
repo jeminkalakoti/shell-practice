890dee95184aeeea8f6ff4b34992b0e10fdaecd9
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-23 15:13:51 -0500

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -10,7 +10,7 @@ sum=$((NUMBER1 + NUMBER2 + NAME)) #arithmetic operation
 echo "Sum is : ${sum}"
 
 #Here the index is 0 and size is 4
-LEADERS=("Jemin, Lakshmi, Ande, Kalakoti")
+LEADERS=("Jemin", "Lakshmi", "Ande", "Kalakoti")
 
 echo "First leader is : ${LEADERS[0]}"
 echo "Number of leaders are : ${#LEADERS[@]}"
