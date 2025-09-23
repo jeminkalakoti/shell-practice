a8ae44d16b8f494b736657b5912f2130d608fcdb
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-23 16:54:20 -0500

    about how to use conditions

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+#even or odd numbers
+echo "Enter a number:"
+read NUMBER
+
+if [ $((NUMBER % 2)) -eq 0 ]; then
+  echo "The number $NUMBER is even"
+else
+  echo "The number $NUMBER is odd"
+fi
