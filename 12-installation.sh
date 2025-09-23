3dc79e5a77356485fdfc2189a1d5ae96331f4c67
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-23 18:48:25 -0500

    Installing package

---
+++ 12-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# TO check if user has a root privilege to install a mysql server
+USER_ID=$(id -u)
+if [$USER_ID -ne 0 ]; then
+    echo "ERROR:: You must have a root privilege to install a mysql"
+fi
+
+dnf install mysql -y
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR:: Installing mysql is FAILED"
+else
+    echo "Installing mysql is SUCCESSFUL"
+fi
