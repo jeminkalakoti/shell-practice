23ffd259c40b27f8ad8eb21cd5dc270b108a43c0
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-23 19:23:34 -0500

    Installing multiple packages

---
+++ 12.1-installation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# TO check if user has a root privilege to install a mysql server
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR:: You must have a root privilege to install mysql"
+    exit 1 # Exit the script if not root
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing mysql is FAILED"
+    exit 1 # Exit the script if installation failed
+else
+    echo "Installing mysql is SUCCESSFUL"
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing nginx is FAILED"
+    exit 1 # Exit the script if installation failed
+else
+    echo "Installing nginx is SUCCESSFUL"
+fi
+
+dnf install mongodb-mongosh -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing mongodb-mongosh is FAILED"
+    exit 1 # Exit the script if installation failed
+else
+    echo "Installing mongodb-mongosh is SUCCESSFUL"
+fi
+
