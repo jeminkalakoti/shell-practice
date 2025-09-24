da5c7a4a96a3e8a6a229d010089e452d15e84951
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-24 17:26:12 -0500

    Installing pacakges using loops

--- 18-loops_installation.sh
+++ 18-loops_installation.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
+#!bin/bash
 
-# TO check if user has a root privilege to install a mysql server
 USER_ID=$(id -u)
 R='\e[0;31m' # Red
 G='\e[0;32m' # Green
 Y='\e[0;33m' # Yellow
 N='\e[0m'    # No Color
 
+LOGS_FOLDER="/var/log/shell-script/"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 ) # Extract script name without extension
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # Define log file path
+
 # -e enables the interpretation of backslash escapes
+
+mkdir -p $LOGS_FOLDER # Create logs folder if not exists
+echo "Script started and executed at : $(date)" | tee -a $LOG_FILE # Log script start time
 
 if [ $USER_ID -ne 0 ]; then
     echo -e "$R ERROR $N:: You must have a root privilege to install packages" 
@@ -16,10 +22,10 @@ fi
 
 VALIDATE(){ # Functions receive arguments like normal scripts
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 ... $R FAILED $N"
+        echo -e "Installing $2 ... $R FAILED $N" | tee -a $LOG_FILE
         exit 1 # Exit the script if installation failed with red color
     else
-        echo -e "Installing $2 ... $G SUCCESSFUL $N" # Print success message in green color
+        echo -e "Installing $2 ... $G SUCCESSFUL $N" | tee -a $LOG_FILE # Print success message in green color
     fi 
 
 }
