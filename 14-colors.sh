c0d0dedcfd0952d36b7cb433884a8f0a6b5b80ff
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-23 20:10:32 -0500

    color coding

--- 14-colors.sh
+++ 14-colors.sh
@@ -5,5 +5,5 @@ G='\e[0;32m' # Green
 Y='\e[0;33m' # Yellow
 N='\e[0m'    # No Color
 
-echo -e "$R Hello World"
-echo "This is red color"
+echo -e "$R Hello World $N" # Print Hello World in red color and reset color
+echo "This is red color" # Print normal color text
