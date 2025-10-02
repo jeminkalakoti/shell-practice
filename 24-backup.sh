5d986aa6786c14828eb7fda6ef661d28e8e21443
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-10-02 13:29:40 -0500

    deleting old logs

--- 24-backup.sh
+++ 24-backup.sh
@@ -8,6 +8,7 @@ N='\e[0m'    # No Color
 
 SOURCE_DIR=$1 # First argument is source directory
 DEST_DIR=$2   # Second argument is destination directory
+DAYS=${3:-14} # Third argument is number of days, default is 14 if not provided
 LOGS_FOLDER="/var/log/shell-script/"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 ) # Extract script name without extension
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # Define log file path
@@ -40,3 +41,21 @@ if [ ! -d $DEST_DIR ]; then
     echo -e "$R ERROR:: Destination $DEST_DIR does not exist $N"
     exit 1 # Exit the script if destination directory does not exist
 fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS) # Find files older than specified days
+
+if [ ! -z "${FILES}" ]; then # Check if FILES is not empty
+    echo  "Files found : $FILES"
+    TIMESTAMP=$(date +%F-%H-%M) # Get current timestamp
+    ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip" # Define zip file name with timestamp
+    echo "zip file name : $ZIP_FILE_NAME"
+    echo $FILES | zip @ -j "$ZIP_FILE_NAME" # Create zip file with the found files
+else
+    echo -e "No files to archive ... "$Y SKIPPING $N"
+
+fi
+
+
+
+
+
