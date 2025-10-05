5123669fa85607e6b87848ba436f1c110aa178af
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-10-05 10:36:38 -0500

    disk usage

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # In projects we use 75%
 
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d '%' -f1)
     PARTITION=$(echo $line | awk '{print $7}')
+    if [ $USAGE -gt DISK_THRESHOLD ]; then
+        echo "High disk usage on $PARTITION: $USAGE"
 done <<< $DISK_USAGE 
